17b56ccba7d84aa2360061c4c434e692b458eae4
PaoloDat <orsopaul007@gmail.com>
2018-08-19 14:30:00 +0300

    Initial Commit

---
+++ src/main/java/ru/vazisu/orsopetclinic/OrsoPetClinicApplication.java
@@ -0,0 +1,12 @@
+package ru.vazisu.orsopetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrsoPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrsoPetClinicApplication.class, args);
+    }
+}
